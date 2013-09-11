b7ffefd1412e17ce5047fa0258ad5a110c73bcd4
aaeron <a.aeron.87@gmail.com>
2013-09-11 09:44:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheAlife::Application.config.secret_token = 'bc6d93abee9488730d03e6651a2c211056cb26a94deb1a76f3a784e978ba124ea75c7d63ed3f0b5ab6a7da237934e388bd7507998e444abd5406c48dcd340b27'
