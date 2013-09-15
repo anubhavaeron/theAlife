2bfb16d1b34e55bf1362f9a50733f493f17de366
aaeron <a.aeron.87@gmail.com>
2013-09-14 20:40:35 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Congratulations, you have now signed in with theAlife.!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 end
